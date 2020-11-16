3eae005cde98a8f0a8a7ea68bb275ffcffd88bdb
Kochise <david.koch@9online.fr>
2020-11-16 20:39:16 +0100

    Remove wavedrom_test

--- wavedrom_test/wavedrom_test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set PATH=%PATH%;%cd%\..\miktex\texmfs\install\miktex\bin\x64;%cd%\..\Msc-generator;%cd%\..\inkscape\bin;%cd%\..\nvm\v12.17.0
-
-start "" cmd /c "npx wavedrom-cli -i mywave.json5 -s mywave.svg"
