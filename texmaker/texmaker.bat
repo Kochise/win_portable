747a7277687eb2a36e1c15ef37b30444a7cf1154
Kochise <david.koch@9online.fr>
2021-07-15 22:31:24 +0200

    Update texmaker (5.0.4 / 200103)

--- texmaker/texmaker.bat
+++ texmaker/texmaker.bat
@@ -1,5 +1,8 @@
 @echo off
 
-set PATH=%PATH%;%cd%\..\miktex\texmfs\install\miktex\bin\x64;%cd%\..\Msc-generator;%cd%\..\inkscape\bin;%cd%\..\nvm\v12.17.0;%cd%\..\perl\perl\bin
+set "PATH=%PATH%;%cd%\..\miktex\texmfs\install\miktex\bin\x64;%cd%\..\Msc-generator;%cd%\..\inkscape\bin;%cd%\..\nvm\v12.17.0;%cd%\..\perl\perl\bin;"
+set "PATH=%PATH:;;=;%"
+set "PATH=%PATH: ;=;%"
+set "PATH=%PATH:; =;%"
 
-start "" "texmaker.exe"
+start "" /d "%cd%" "texmaker.exe"
