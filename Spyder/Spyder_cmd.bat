6394a45ca760ec0fd0080a4864333bf0caa4f032
Kochise <david.koch@9online.fr>
2021-10-27 13:52:58 +0200

    Update Spyder runner

--- Spyder/Spyder_cmd.bat
+++ Spyder/Spyder_cmd.bat
@@ -2,10 +2,23 @@
 
 set "PYSDL2_DLL_PATH=%cd%\lib"
 
-set "PY_HOME=%cd%\Python"
-set "PYTHONPATH=%PY_HOME%;%PY_HOME%\Lib;%PY_HOME%\Scripts"
+set "PYTHONHOME=%cd%\Python"
+set "PYTHONPATH=%PYTHONHOME%;%PYTHONHOME%\Lib;%PYTHONHOME%\Scripts"
 
 set "PATH=%PATH%;%PYTHONPATH%;"
 set "PATH=%PATH:;;=;%"
 
-start "" /d "%PY_HOME%" "cmd" ""
+start "" /d "%PYTHONHOME%" "cmd" ""
+
+rem call set "path=%path:%_OLD_VIRTUAL_PYTHONPATH%=%
+rem set "PYTHONHOME=%cd%\Scripts"
+rem set "PYTHONPATH=%PYTHONHOME%;%cd%\Lib"
+rem set "path=%path%;%PYTHONPATH%;"
+rem set "PATH=%PATH:;;=;%"
+rem set "PATH=%PATH:;;=;%"
+
+rem pip install virtualenvwrapper-win
+rem mkvirtualenv venv
+rem workon venv
+rem deactivate
+rem rmvirtualenv venv
--- Spyder/Spyder_pip_reinstall.bat
+++ Spyder/Spyder_pip_reinstall.bat
@@ -1,15 +1,18 @@
 @echo off
-echo Starting pip installation process...
+echo Starting...
 echo.
 
+rem === Remove fake links for Windows Store
+del "%LOCALAPPDATA%\Microsoft\WindowsApps\Python*.exe"
+
+rem === Copy SDL2 (for Payton)
 set "PYSDL2_DLL_PATH=%cd%\lib"
-mkdir "%PYSDL2_DLL_PATH%"
-copy /y "%cd%\SDL2.dll" "%PYSDL2_DLL_PATH%" 2>nul
+copy /y "%cd%\SDL2.dll" "%PYSDL2_DLL_PATH%" >nul
 
 set "PIP_PATH="
 rem set "PIP_PATH=-t ^"%cd%\pkgs^""
 set "PIP_OPTS="
-rem set "PIP_OPTS=--no-cache-dir"
+set "PIP_OPTS=--no-cache-dir"
 rem --no-cache-dir
 rem --use-deprecated=legacy-resolver
 
@@ -22,39 +25,66 @@ set "PATH=%PATH: ;=;%"
 set "PATH=%PATH:; =;%"
 rem echo %PATH%
 
+rem === Update Setuptools dependencies
+copy /y "%cd%\distribute_setup.py" "%PY_HOME%\Lib" >nul
+
+rem === Install and update pip
 rem curl https://bootstrap.pypa.io/get-pip.py >get-pip.py
 "%PY_HOME%\python" get-pip.py
 call :pip_install pip
 rem pip cache dir
 rem pip cache purge
 
+rem === VENV and stuff (doesn't quite work as expected though)
+call :pip_install virtualenv
+call :pip_install virtualenvwrapper-win
+rem cd %USERPROFILE%\Envs
+rem cd %WORKON_HOME%
+
+rem === Update Spyder and stuff (doesn't quite work as expected though)
 call :pip_install pretty_errors
+call :pip_install bcrypt
 rem call :pip_install spyder-terminal
 
+rem === OpenGL framework (actually pretty cool one)
 rem call :pip_install pyrsistent
 rem call :pip_install python-slugify
-rem call :pip_install bcrypt
 call :pip_install payton
 
+rem === CAN stuff (low level and not async)
 call :pip_install python-can
 call :pip_install python-ics
 call :pip_install python_can_viewer
 
+rem === Signal stuff
 call :pip_install filelock
 call :pip_install similaritymeasures
+rem call :pip_install construct
 
-call :pip_install dearpygui
+rem === Gui related (Dear PyGui, but don't 0.8+, it's even worse)
+call :pip_install "dearpygui<0.7"
 
+rem === Gui related (Enaml, declarative and functional oriented)
 call :pip_install rtree
 call :pip_install intervaltree
+call :pip_install traits
+call :pip_install vtk
 call :pip_install enaml
 
+rem === Gui related (Enamlx, maybe outdated a bit)
 rem call :pip_install pythonocc-core
 call :pip_install pyqtgraph
-rem call :pip_install "QScintilla==2.11.3"
+rem call :pip_install "QScintilla==2.11.0"
+rem call :pip_install "QScintilla<2.12.1"
 call :pip_install enamlx
 
-rem call :pip_install construct
+rem === Graphic related
+call :pip_install bokeh
+rem call :pip_install pygame
+rem call :pip_install flexx
+
+rem === Tui related
+call :pip_install beautifultable
 
 goto :eof
 
