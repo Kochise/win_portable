b201bc59f68a6d1d529f9a20b61e6b91964e8782
Kochise <david.koch@9online.fr>
2022-11-11 09:16:19 +0100

    Update Startup folder opener

--- Startup/open_startup_folder.bat
+++ Startup/open_startup_folder.bat
@@ -1,1 +1,1 @@
-cd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+start "" explorer "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
